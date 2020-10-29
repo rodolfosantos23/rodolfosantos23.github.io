fc7bdfd3900191989e5587abca998860a0c25226
rodolfosantos23 <rodolfosantos@msn.com>
2020-10-28 23:10:28 -0300

    Site em ReactJS

---
+++ precache-manifest.7945dc9028e7aa80915446bea8064249.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43ba6fe56fadff8f4dac604d2a2e087c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc677f755d1d369e25bb",
+    "url": "/static/css/main.0ecc7309.chunk.css"
+  },
+  {
+    "revision": "0fe6b6f8daa1670d1169",
+    "url": "/static/js/2.29325d07.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.29325d07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc677f755d1d369e25bb",
+    "url": "/static/js/main.147c13c4.chunk.js"
+  },
+  {
+    "revision": "7ba67cb0e7f766e08ed5",
+    "url": "/static/js/runtime-main.fb08c56a.js"
+  },
+  {
+    "revision": "4ee4ee61221714642c6bb8b38936c42e",
+    "url": "/static/media/mongo-icon.4ee4ee61.png"
+  },
+  {
+    "revision": "094a6a02a02607d64ef338c9275f72a1",
+    "url": "/static/media/node-icon.094a6a02.png"
+  },
+  {
+    "revision": "146823f007fdb2637b8139de42d4a3f0",
+    "url": "/static/media/php-icon.146823f0.png"
+  },
+  {
+    "revision": "0f1ca09a8e5d1bb5e160e60f9fb97f89",
+    "url": "/static/media/react-icon.0f1ca09a.png"
+  },
+  {
+    "revision": "706444f902710f330a74a2f6cf43c7b5",
+    "url": "/static/media/slider_1.706444f9.png"
+  }
+]);
