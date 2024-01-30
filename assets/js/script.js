59a709a87e096525fe49241599d65330a75a3bee
Rodolfo Santos <rodolfosantos23@gmail.com>
2024-01-29 21:10:44 -0300

    Novo tema - Novo Site

---
+++ assets/js/script.js
@@ -0,0 +1,395 @@
+(function ($) {
+  "use strict";
+
+  /* ============================================================ */
+  /* PRELOADER START
+  /* ============================================================ */
+  setTimeout(function () {
+    var e = !!/Android|webOS|iPhone|iPad|iPod|BlackBerry/i.test(
+        navigator.userAgent
+      ),
+      s = document.getElementById("preloader");
+    e
+      ? s && s.parentNode && s.parentNode.removeChild(s)
+      : (setTimeout(function () {
+          s.classList.add("preloaded");
+        }, 1000),
+        setTimeout(function () {
+          s && s.parentNode && s.parentNode.removeChild(s);
+        }, 2000));
+  }, 1000);
+  /* Preloader End */
+
+  /* ============================================================ */
+  /* MOBILE MENU START
+    /* ============================================================ */
+  function mobile_menu(selector, actionSelector) {
+    var mobile_menu = $(selector);
+    mobile_menu.on("click", function () {
+      $(selector).toggleClass("is-menu-open");
+    });
+
+    var hamburgerbtn = $(selector);
+    hamburgerbtn.on("click", function () {
+      $(actionSelector).toggleClass("is-menu-open");
+    });
+
+    $(document).on("click", function (e) {
+      var selectorType = $(actionSelector).add(mobile_menu);
+      if (
+        selectorType.is(e.target) !== true &&
+        selectorType.has(e.target).length === 0
+      ) {
+        $(actionSelector).removeClass("is-menu-open");
+        $(selector).removeClass("is-menu-open");
+      }
+    });
+    $(".mobile-menu .main-menu a, .menu-overlay").on("click", function (e) {
+      $(actionSelector).removeClass("is-menu-open");
+      $(selector).removeClass("is-menu-open");
+    });
+  }
+  mobile_menu(
+    ".menu_toggle, .close-menu ",
+    ".mobile-menu, .minfo__app, .menu-overlay"
+  );
+  /* Mobile menu End */
+
+  /* ============================================================ */
+  /* Scrollit Scrollspy start
+    /* ============================================================ */
+  function checkSize() {
+    var width = $(window).width();
+    if (width < 1240) {
+      $.scrollIt({
+        scrollTime: 1000,
+        topOffset: -55,
+      });
+    } else {
+      $.scrollIt({
+        scrollTime: 1000,
+        topOffset: 0,
+      });
+    }
+  }
+  $(document).ready(function () {
+    $(window).on("resize", checkSize);
+    checkSize();
+  });
+  /* Scrollit Scrollspy End */
+
+  /* ============================================================ */
+  /* Counterup Fun-facts start
+    /* ============================================================ */
+  $(".counters .number span").counterUp({
+    delay: 10,
+    time: 1500,
+  });
+  /* Counterup Fun-facts End */
+
+  /* ============================================================ */
+  /* Hero Section Logo Slider start
+    /* ============================================================ */
+  let logoSlider = new Swiper(".logo-slider .swiper", {
+    spaceBetween: 30,
+    slidesPerView: 3,
+    loop: true,
+    speed: 3000,
+    autoplay: {
+      delay: 0,
+      disableOnInteraction: false,
+    },
+    allowTouchMove: false,
+    breakpoints: {
+      576: {
+        slidesPerView: 3,
+      },
+      768: {
+        slidesPerView: 4,
+      },
+      1200: {
+        slidesPerView: 4,
+      },
+      1400: {
+        slidesPerView: 5,
+      },
+    },
+  });
+
+  /* ============================================================ */
+  /* Skills Slider start
+    /* ============================================================ */
+  var skillSlider = new Swiper(".skills-slider .swiper", {
+    spaceBetween: 30,
+    slidesPerView: 2,
+    loop: !1,
+    speed: 800,
+    breakpoints: {
+      600: {
+        slidesPerView: 3,
+      },
+      800: {
+        slidesPerView: 4,
+      },
+    },
+    navigation: {
+      nextEl: "#skill .button-next",
+      prevEl: "#skill .button-prev",
+    },
+    on: {
+      init: function () {
+        $(".skills-slider-navigation .counter").html(
+          '<span class="text-theme">' +
+            (this.realIndex + 1) +
+            "</span>" +
+            "/" +
+            this.slides.length
+        );
+      },
+      slideChange: function () {
+        $(".skills-slider-navigation .counter").html(
+          '<span class="text-theme">' +
+            (this.realIndex + 1) +
+            "</span>" +
+            "/" +
+            this.slides.length
+        );
+      },
+    },
+  });
+  // Skills Slider End
+
+  /* ============================================================ */
+  /* Testimonial Slider start
+    /* ============================================================ */
+  let testimonialSlider = new Swiper(".testimonial-slider .swiper", {
+    spaceBetween: 30,
+    slidesPerView: 1,
+    loop: false,
+    speed: 800,
+    breakpoints: {
+      768: {
+        slidesPerView: 2,
+      },
+    },
+    navigation: {
+      nextEl: ".testimonial-slider-navigation .button-next",
+      prevEl: ".testimonial-slider-navigation .button-prev",
+    },
+    on: {
+      init: function () {
+        $(".testimonial-slider-navigation .counter").html(
+          '<span class="text-theme">' +
+            (this.realIndex + 1) +
+            "</span>" +
+            "/" +
+            this.slides.length
+        );
+      },
+      slideChange: function () {
+        $(".testimonial-slider-navigation .counter").html(
+          '<span class="text-theme">' +
+            (this.realIndex + 1) +
+            "</span>" +
+            "/" +
+            this.slides.length
+        );
+      },
+    },
+  });
+  // Testimonial Slider End
+
+  /* ============================================================ */
+  /* Progressbar start
+    /* ============================================================ */
+  var CroWey = $(".progressbar");
+  if (CroWey.length > 0) {
+    CroWey.waypoint(
+      function () {
+        $(".bar").each(function () {
+          $(this)
+            .find(".progress-content")
+            .animate(
+              {
+                width: $(this).attr("data-percentage"),
+              },
+              2000
+            );
+        });
+      },
+      {
+        offset: "100%",
+      }
+    );
+  }
+  // Progressbar End
+
+  /* ============================================================ */
+  /* Custom Cursor start
+    /* ============================================================ */
+  if ($(".custom_cursor").length) {
+    var cursor = document.querySelector(".custom_cursor_one");
+    var cursorInner = document.querySelector(".custom_cursor_two");
+    var anchors = document.querySelectorAll("a");
+
+    document.addEventListener("mousemove", function (e) {
+      var x = e.clientX;
+      var y = e.clientY;
+      cursor.style.transform = `translate3d(calc(${e.clientX}px - 50%), calc(${e.clientY}px - 50%), 0)`;
+    });
+    document.addEventListener("mousemove", function (e) {
+      var x = e.clientX;
+      var y = e.clientY;
+      cursorInner.style.left = x + "px";
+      cursorInner.style.top = y + "px";
+    });
+    document.addEventListener("mousedown", function () {
+      cursor.classList.add("click");
+      cursorInner.classList.add("custom_cursor_hover");
+    });
+    document.addEventListener("mouseup", function () {
+      cursor.classList.remove("click");
+      cursorInner.classList.remove("custom_cursor_hover");
+    });
+    anchors.forEach((item) => {
+      item.addEventListener("mouseover", () => {
+        cursor.classList.add("custom_cursor_hover");
+      });
+      item.addEventListener("mouseleave", () => {
+        cursor.classList.remove("custom_cursor_hover");
+      });
+    });
+  }
+  // Custom Cursor End
+
+  /* ============================================================ */
+  /* Animated Circle Progress start
+    /* ============================================================ */
+  function animateElements() {
+    $(".minfo__sidebar .progressCircle").each(function () {
+      var elementPos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      var percent = $(this).find(".circle").attr("data-percent");
+      var animate = $(this).data("animate");
+      if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+        $(this).data("animate", true);
+        $(this)
+          .find(".circle")
+          .circleProgress({
+            startAngle: -Math.PI / 30,
+            value: percent / 100,
+            thickness: 2,
+            lineCap: "round",
+            emptyFill: "#777777",
+            // fill: '#0272c6',
+            fill: "#00bc91",
+            size: $(".circle").width(),
+          })
+          .on(
+            "circle-animation-progress",
+            function (event, progress, stepValue) {
+              $(this)
+                .parent()
+                .find(".label")
+                .html((stepValue * 100).toFixed(0) + "%");
+            }
+          )
+          .stop();
+      }
+    });
+    $(".skills-slider .progressCircle").each(function () {
+      var elementPos = $(this).offset().top;
+      var topOfWindow = $(window).scrollTop();
+      var percent = $(this).find(".circle").attr("data-percent");
+      var animate = $(this).data("animate");
+      if (elementPos < topOfWindow + $(window).height() - 30 && !animate) {
+        $(this).data("animate", true);
+        $(this)
+          .find(".circle")
+          .circleProgress({
+            startAngle: -Math.PI / 2,
+            value: percent / 100,
+            thickness: 1.5,
+            lineCap: "round",
+            emptyFill: "#777777",
+            // fill: '#0272c6',
+            fill: "#00bc91",
+            size: $(".skills-slider .circle").width(),
+          })
+          .on(
+            "circle-animation-progress",
+            function (event, progress, stepValue) {
+              $(this)
+                .parent()
+                .find(".label")
+                .html((stepValue * 100).toFixed(0) + "%");
+            }
+          )
+          .stop();
+      }
+    });
+  }
+  setTimeout(function () {
+    animateElements();
+    $(window).scroll(animateElements);
+  }, 2500);
+
+  $(document).ready(function () {
+    const firstJobDate = new Date("2012-01-01");
+    const birthDate = new Date("1987-02-23");
+    const today = new Date();
+    const age = today.getFullYear() - birthDate.getFullYear();
+    const experience = today.getFullYear() - firstJobDate.getFullYear();
+    $("#idade").html(age);
+    $("#experience").html(experience - 1);
+
+    let postsCount = 0;
+    $.get(
+      "https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@rodolfosantos23"
+    ).done(function (data) {
+      data.items.map((post) => {
+        const formattedDate = new Date(post.pubDate);
+        const options = { year: "numeric", month: "long", day: "numeric" };
+        const formattedDateStr = formattedDate.toLocaleDateString(
+          "pt-BR",
+          options
+        );
+
+        let html = "";
+        html +=
+          "<div class='grid md:gap-2 grid-cols-12 overflow-hidden article group bg-metalBlack items-center rounded-2xl p-3.5'>";
+        html +=
+          '<div class="flex col-span-12 overflow-hidden thumbnail sm:col-span-6 md:col-span-5">';
+        html += `<a href="${post.guid}" target="_blank" class="block w-full overflow-hidden rounded-xl">`;
+        html += `<img src="assets/img/medium.jpg" class="object-cover object-center w-full h-full min-h-[288px] transition-all duration-300 ease-in-out max-h-60 md:min-h-60 group-hover:scale-105" alt="${post.title}" title="${post.title} - Medium">`;
+        html += "</a>";
+        html += "</div>";
+
+        html +=
+          '<div class="relative flex flex-col col-span-12 px-3 pt-6 pb-2 md:p-5 post-content sm:col-span-6 md:col-span-7">';
+        html += '<div class="flex items-center gap-5">';
+        html += '<div class="text-sm font-medium tags">';
+        html += `<a href="https://rodolfosantos23.medium.com/" target='_blank' class="transition-colors hover:text-theme">${post.author}</a>, <span class="post_date">${formattedDateStr}</span>`;
+        html += "</div>";
+        html += "</div>";
+
+        html += '<div class="post-title mt-3 md:mt-4.5 mb-6 md:mb-8">';
+        html += `<a href="${post.guid}" target="_blank" class="text-xl font-semibold leading-normal text-white transition-colors line-clamp-2 2xl:text-2xl 2xl:leading-normal hover:text-theme">${post.title}</a>`;
+        html += "</div>";
+
+        html += '<div class="read-details">';
+        html += `<a href="${post.guid}" target="_blank" class="inline-flex items-center gap-2 bg-metalBlack border border-theme text-theme text-sm py-3.5 px-6 rounded-3xl leading-none transition-all duration-300 hover:bg-themeHover hover:border-themeHover font-medium hover:text-white">Ler Mais</a>`;
+        html += "</div>";
+
+        html += "</div>";
+        html += "</div>";
+        $("#posts").append(html);
+
+        postsCount++;
+        if (postsCount >= 3) {
+          return false;
+        }
+      });
+    });
+  });
+})(jQuery);
---
+++ tailwind.config.js
@@ -0,0 +1,143 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode: ['class'],
+    content: ['*', './node_modules/tw-elements/dist/js/**/*.js'],
+    theme: {
+        extend: {
+            maxWidth: {
+                container: '76.25rem',
+                content: '58.75rem',
+                sidebar: '22.75rem',
+                mobilemenu: '20rem',
+                sectionTitle: '37rem',
+                '50rem': '50rem',
+                '60rem': '60rem',
+                '65rem': '65rem',
+            },
+            width: {
+                4.5: '1.125rem', // 18px
+                7.5: '1.875rem', // 30px
+                13: '3.125rem', // 50px
+                15: '3.75rem', // 60px
+                18: '4.5rem', // 72px
+            },
+            height: {
+                4.5: '1.125rem', // 18px
+                7.5: '1.875rem', // 30px
+                13: '3.125rem', // 50px
+                15: '3.75rem', // 60px
+                18: '4.5rem', // 72px
+            },
+            padding: {
+                4.5: '1.125rem', // 18px
+                7.5: '1.875rem', // 30px
+                13: '3.125rem', // 50px
+                15: '3.75rem', // 60px
+                18: '4.5rem', // 72px
+            },
+            margin: {
+                4.5: '1.125rem', // 18px
+                7.5: '1.875rem', // 30px
+                13: '3.125rem', // 50px
+                15: '3.75rem', // 60px
+                18: '4.5rem', // 72px
+            },
+            fontFamily: {
+                sans: ["'Poppins', sans-serif"],
+            },
+            fontSize: {
+                lead: '2.45rem', // 40px
+                regular: '0.94rem', //15px
+            },
+            lineHeight: {
+                1.15: '1.15',
+                1.27: '1.27',
+                1.875: '1.875',
+            },
+            borderRadius: {
+                '4xl': '2.5rem', // 40px
+            },
+            colors: {
+                textColor: 'var(--textColor)',
+                theme: 'var(--themeColor)',
+                themeHover: 'var(--themeHoverColor)',
+                power__black: 'var(--power__black)',
+                nightBlack: 'var(--nightBlack)',
+                metalBlack: 'var(--metalBlack)',
+                lightOrange: 'var(--lightOrange)',
+                greyBlack: 'var(--greyBlack)',
+                greyBlack200: 'var(--greyBlack200)',
+            },
+            spacing: {
+                4.5: '1.125rem', // 18px
+                7.5: '1.875rem', // 30px
+                13: '3.125rem', // 50px
+                15: '3.75rem', // 60px
+                18: '4.5rem', // 72px
+            },
+            keyframes: {
+                topToBottom: {
+                    '0%': {
+                        top: '-4rem',
+                    },
+                    '100%': {
+                        top: '100%',
+                    },
+                },
+                bottomToTop: {
+                    '0%': {
+                        bottom: '-4rem',
+                    },
+                    '100%': {
+                        bottom: '100%',
+                    },
+                },
+                lineheight: {
+                    '0%': {
+                        height: '0%',
+                    },
+                    'to': {
+                        height: '100%',
+                    }
+                },
+                lineround: {
+                    'transform': {
+                        height: 'translateY(-100%)',
+                    },
+                    'transform': {
+                        height: 'translateY(200%)',
+                    }
+                },
+                preloadedzero: {
+                    '0%': {
+                        width: '50%',
+                    },
+                    'to': {
+                        width: '0',
+                    }
+                },
+            },
+            animation: {
+                'spin-slow': 'spin 6s linear infinite',
+                top_bottom: 'topToBottom 25s linear infinite',
+                bottom_top: 'bottomToTop 25s linear infinite',
+                lineheight: 'lineheight 1s ease-in-out 0s forwards',
+                lineround: 'lineround 1.2s linear 2s infinite',
+                preloadedzero: 'preloadedzero 0.3s ease-in-out 0.5s forwards',
+            },
+            boxShadow: {
+                dots_glow: '0 0 0 .5rem rgba(255, 255, 255, 0.15)',
+                '3xl': '0px 0px 30px 0px rgba(0, 0, 1, 0.05)',
+                theme_shadow: '0px 5px 10px 0px rgba(0, 188, 145, 0.30)',
+            },
+            zIndex: {
+                1: '1',
+                999: '999',
+            },
+            blur: {
+                xs: '1.5px',
+            },
+        },
+    },
+    plugins: [require('tw-elements/dist/plugin.cjs')],
+};
